62d99e4cc7177fe5ab32d1fb66eed48dbc88549e
David Zhao <bravetheheat1998@gmail.com>
2019-10-31 11:51:28 -0400

    Update origin-request.js

--- origin-request.js
+++ origin-request.js
@@ -5,20 +5,20 @@
 exports.handler = (event, context, callback) => {
     const request = event.Records[0].cf.request;
 
-    if (request.headers['x-prerender-token']) {
+    if (request.headers['x-prerender-cachebuster']) {
          const domainName = 'service.prerender.io';  // Or your Prerender.io-compatible server
          const host = request.headers['host']['value'];
             request.origin = {
                 custom: {
                     domainName: domainName,
-                    path: `/${host}`,
+                    path: `/https%3A%2F%2F${host}`,
                     keepaliveTimeout: 5,
                     port: 443,
                     protocol: "https",
                     readTimeout: 20,
                     sslProtocols: [
                         "TLSv1.1",
-                        "TLSv1.2"
+                        "TLSv1.2",
                     ]
 
                 }
