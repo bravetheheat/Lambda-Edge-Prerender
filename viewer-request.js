bf652cefc181c7532d7f7315a75376e2cd422760
David Zhao <bravetheheat1998@gmail.com>
2019-10-31 11:48:13 -0400

    Update viewer-request.js

--- viewer-request.js
+++ viewer-request.js
@@ -13,8 +13,10 @@ exports.handler = async (event, context, callback) => {
         var prerender = /Feedfetcher\-Google|bingbot|yandex|baiduspider|Facebot|facebookexternalhit|twitterbot|rogerbot|linkedinbot|embedly|quora link preview|showyoubot|outbrain|pinterest|slackbot|vkShare|W3C_Validator/i.test(user_agent[0].value);
         prerender = prerender || /_escaped_fragment_/.test(request.querystring);
         
+        /* Not needed if origin settings on Cloudfront have been set to only route *.html files to the lambda@edge function
         // Don't redirect requests for media or script files.
         prerender = prerender && !/\.(js|css|xml|less|png|jpg|jpeg|gif|pdf|doc|txt|ico|rss|zip|mp3|rar|exe|wmv|doc|avi|ppt|mpg|mpeg|tif|wav|mov|psd|ai|xls|mp4|m4a|swf|dat|dmg|iso|flv|m4v|torrent|ttf|woff|svg|eot)$/i.test(request.uri);
+        */
         
         if (prerender) {
              // Set request header with Prerender API token
